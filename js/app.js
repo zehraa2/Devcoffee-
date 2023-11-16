55d5c5f8bf30be4691e2b50fe605aa72f6e4cb42
zehraa2 <85738977+zehraa2@users.noreply.github.com>
2023-11-16 16:33:53 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -333,4 +333,27 @@ const target = document.getElementById('target');
   
   target.innerHTML = 'Test content written to ' + file.name + '.';
 }
+if ('storage' in navigator && 'estimate' in navigator.storage) {
+  navigator.storage.estimate()
+    .then(estimate => {
+      document.getElementById('usage').innerHTML = estimate.usage;
+      document.getElementById('quota').innerHTML = estimate.quota;
+      document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota).toFixed(0);
+    });
+}
 
+if ('storage' in navigator && 'persisted' in navigator.storage) {
+  navigator.storage.persisted()
+    .then(persisted => {
+      document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+    });
+}
+
+function requestPersistence() {
+  if ('storage' in navigator && 'persist' in navigator.storage) {
+    navigator.storage.persist()
+      .then(persisted => {
+        document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+      });
+  }
+}
