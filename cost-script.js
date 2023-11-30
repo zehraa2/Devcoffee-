8a65450d147c4979f4451fdd86e472d18a38ae2b
zehraa2 <85738977+zehraa2@users.noreply.github.com>
2023-11-30 17:06:51 +0100

    Update cost-script.js

--- cost-script.js
+++ cost-script.js
@@ -26,34 +26,3 @@ document.getElementById('costForm').addEventListener('submit', function(event) {
 const savedData = localStorage.getItem('costEntry');
 console.log('Gespeicherte Daten:', JSON.parse(savedData));
 
-// Funktion zum Generieren der Tabelle mit gespeicherten Kosten
-function generateTable() {
-  const costTableBody = document.getElementById('costTableBody');
-  const savedCosts = JSON.parse(localStorage.getItem('gespeicherteKosten')) || [];
-
-  if (savedCosts.length > 0) {
-    savedCosts.forEach(cost => {
-      const row = costTableBody.insertRow();
-      const descriptionCell = row.insertCell(0);
-      const dateCell = row.insertCell(1);
-      const categoryCell = row.insertCell(2);
-      const amountCell = row.insertCell(3);
-
-      descriptionCell.textContent = cost.description;
-      dateCell.textContent = cost.date;
-      categoryCell.textContent = cost.category;
-      amountCell.textContent = cost.amount;
-    });
-  } else {
-    // Nachricht, falls keine Daten vorhanden sind
-    const row = costTableBody.insertRow();
-    const messageCell = row.insertCell();
-    messageCell.colSpan = 4;
-    messageCell.textContent = 'Keine gespeicherten Kosten vorhanden';
-  }
-}
-
-// Tabelle generieren, wenn die Seite geladen wird
-window.onload = function() {
-  generateTable();
-};
